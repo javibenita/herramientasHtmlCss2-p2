6c72752ff6e2f7d3ab6d04b09d62b9bed6c3d19d
Javier Benita Crespo <javibenita@gmail.com>
2025-04-22 23:37:40 +0200

    primer commit

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,12 +5,15 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
+import * as bootstrap from "bootstrap";
 
 /**
  * Write any other JavaScript below
  */
 
-+( function() {
++(function () {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
-} )();
+  console.log(bootstrap);
+  console.log(bootstrap.Tooltip);
+})();
